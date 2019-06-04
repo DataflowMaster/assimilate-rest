449f7c4dcdd582370d00879c1d1efec65df538fd
DataFlowMaster <iskconsalaz@gmail.com>
2019-06-04 01:05:57 +0100

    testing config

---
+++ test/test.spec.js
@@ -0,0 +1,9 @@
+import { expect } from "chai"
+
+describe("index test", () => {
+    it("should say Hello guys!", () => {
+        const str = "Hello guys!";
+        expect(str).to.equal("Hello guys!")
+    })
+})
+
