ea5d3690c77f5040f460a6ab66acdb4b12933253
icontreras <israel.contreras@lastminute.com>
2019-06-10 00:19:02 +0200

    Config for ui

--- src/domain/AutthenticateEntity/postAuth.js
+++ src/domain/AutthenticateEntity/postAuth.js
@@ -11,6 +11,9 @@ export function postAuth({server,generateToken,authCredentials,renderWhatever}){
         authCredentials(req.body.username,req.body.password, data => {
             if(data){
                 let token = generateToken(data);
+                token.username = data.username;
+                token.idprofessor = data.professor_idprofessor;
+                token.idinstitucion = data.professor_institution_idinstitution;
                 renderWhatever(res,token);
             }else{
                 renderWhatever(res,{error: "User or password incorrect"})
